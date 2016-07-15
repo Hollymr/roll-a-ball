ae016a4e9f4e06c9a8b10ec35f5502d2f238d6e5
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-15 14:49:53 -0400

    inital commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+        // camera position - player position
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        // player position + offset to = camera position
+        transform.position = player.transform.position + offset;
+	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;// allows you to change speed from the inspector in unity 
+
+    void Start()
+    {   // grabs rigidbody component from unity 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(movehorizontal, 0, movevertical);
+        // add force needs a vector3 which we grabbed from ^^
+        rb.AddForce(force * speed );// multiplying force * speed to quicken speed of roll 
+       
+    }
+}
